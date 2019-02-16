6d02e98f3779df40035c43175709a49c482ba699
AviKKi <atul7555@gmail.com>
2019-02-16 21:46:13 +0530

    Server configs done

--- ims_backend/settings.py
+++ ims_backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'vx@-lzs_x9c4#esa-=dfx@s7cy6me8$jvq(7dg7=k--m(y(3ov'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -161,3 +161,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
