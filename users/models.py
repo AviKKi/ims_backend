bd0bebccdd2814df3bc60c9b8afca10481401be7
AviKKi <atul7555@gmail.com>
2019-02-17 15:40:15 +0530

    User model serializers updated

--- users/models.py
+++ users/models.py
@@ -14,9 +14,12 @@ class CustomUser(AbstractUser):
         (STARTUP, "Startup"),
     )
     name = models.CharField(blank=True, max_length=255)
+    product = models.CharField(blank=True, max_length=150)
     profile_url=models.CharField(blank=True,max_length=200)
     usertype = models.CharField(max_length=1, choices=user_type_choices, default=STARTUP)
     bio=models.CharField(blank=True,max_length=100)
+    locations = models.CharField(blank=True, max_length=255)
+    markets = models.CharField(blank=True, max_length=255)
     contact=models.CharField(blank=True,max_length=15,)
     experience=models.CharField(blank=True,max_length=100)
     education=models.CharField(blank=True,max_length=200)
--- users/serializers.py
+++ users/serializers.py
@@ -4,4 +4,18 @@ from . import models
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.CustomUser
-        fields = ('email', 'username', 'usertype', 'bio', 'contact')
+        fields = (
+            'name',
+            'product',
+            'profile_url',
+            'usertype',
+            'bio',
+            'locations',
+            'markets',
+            'contact',
+            'education',
+            'experience',
+            'approved',
+            'email',
+            'username',
+            )
